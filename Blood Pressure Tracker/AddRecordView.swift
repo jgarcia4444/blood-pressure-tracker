afaf68bd30a666c83374af20a0ac706b3deed304
Jake Garcia <bballwiz4@gmail.com>
2021-03-06 23:21:04 -0800

    Added in a notes section.

--- Blood Pressure Tracker/AddRecordView.swift
+++ Blood Pressure Tracker/AddRecordView.swift
@@ -10,8 +10,9 @@ import SwiftUI
 
 struct AddRecordView: View {
     @Environment(\.managedObjectContext) var managedObjectContext
-    @State private var systolic : Int16? = 0
-    @State private var diastolic : Int16? = 0
+    @State private var systolic = ""
+    @State private var diastolic = ""
+    @State private var notes = ""
     @State private var displayEntryConfirmAlert = false
     @Environment(\.presentationMode) var presentationMode
     var body: some View {
@@ -19,67 +20,75 @@ struct AddRecordView: View {
             LinearGradient(gradient: Gradient(colors: [.white, .gray]), startPoint: .topLeading, endPoint: .bottomTrailing)
             ScrollView {
                 VStack {
-//                    Spacer()
                     VStack {
-                        Section {
-                            Text("Systolic")
-                            Picker(selection: $systolic, label: Text("Systolic")) {
-                                ForEach(0..<241) {
-                                    Text("\($0)")
+                        HStack {
+                            Spacer()
+                            VStack {
+                                TextField("Systolic", text: $systolic)
+                                    .keyboardType(.numberPad)
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                            }
+                            VStack {
+                                TextField("Diastolic", text: $diastolic)
+                                    .keyboardType(.numberPad)
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
                                 }
+                                Spacer()
                             }
-                            .pickerStyle(WheelPickerStyle())
+                        .padding(.bottom, 20)
+                        VStack {
+                                Text("Notes")
+                                TextEditor(text: $notes)
+                                    .frame(width: 350, height: 50 )
+                                    .cornerRadius(5)
+                                Text("Was there a specific reason you recorded your blood pressure")
+                                    .font(.caption)
+                            }
                         }
-                        Section {
-                            Text("Diastolic")
-                            Picker(selection: $diastolic, label: Text("Diastolic")) {
-                                ForEach(0..<241) {
-                                    Text("\($0)")
+                        .padding(.top, 125)
+                        VStack {
+                            Button(action: {
+                                withAnimation {
+                                    self.displayEntryConfirmAlert = true
                                 }
-                            }
-                            .pickerStyle(WheelPickerStyle())
-                        }
-                        
-                    }
-                    .padding(.top, 125)
-                    VStack {
-                        Button(action: {
-                            withAnimation {
-                                self.displayEntryConfirmAlert = true
-                            }
-                            }) {
-                            Text("Add Record")
-                                .padding()
-                            .overlay(
-                                Capsule()
-                                    .stroke(lineWidth: 3)
+                                }) {
+                                Text("Add Record")
+                                    .padding()
+                                    .overlay(
+                                        Capsule()
+                                            .stroke(lineWidth: 3)
+                                            .foregroundColor(.black)
+                                    )
+                                    .background(LinearGradient(gradient: Gradient(colors: [.white, .gray]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                                    .clipShape(Capsule())
                                     .foregroundColor(.black)
-                            )
-                                .background(LinearGradient(gradient: Gradient(colors: [.white, .gray]), startPoint: .topLeading, endPoint: .bottomTrailing))
-                                .clipShape(Capsule())
-                                .foregroundColor(.black)
-                                .shadow(color: .black, radius:  3, x: 0, y: 3)
+                                    .shadow(color: .black, radius:  3, x: 0, y: 3)
+                            }
+                            .animation(.easeOut)
+                            .disabled(self.systolic.count == 0 || self.diastolic.count == 0)
                         }
-                        .animation(.easeOut)
-                        .disabled(self.systolic == 0 || self.diastolic == 0)
                     }
-//                    Spacer()
                 }
             }
-        }
-        .navigationBarTitle("Blood Pressure", displayMode: .large)
-        .edgesIgnoringSafeArea(.all)
-        .alert(isPresented: $displayEntryConfirmAlert) {
-            Alert(title: Text("New BP Entry"), message: Text("Your blood pressure input of \(systolic!) / \(diastolic!) (systolic/diastolic) will be saved when confirmed."), primaryButton: .default(Text("Confirm"), action: {
-                self.saveBP()
-            }), secondaryButton: .cancel(Text("Cancel")) )
-        }
+            .navigationBarTitle("Blood Pressure", displayMode: .large)
+            .edgesIgnoringSafeArea(.all)
+            .alert(isPresented: $displayEntryConfirmAlert) {
+                Alert(title: Text("New BP Entry"), message: Text("Your blood pressure input of \(systolic) / \(diastolic) (systolic/diastolic) will be saved when confirmed."), primaryButton: .default(Text("Confirm"), action: {
+                    self.saveBP()
+                }), secondaryButton: .cancel(Text("Cancel")) )
+            }
     }
     
     func saveBP() {
         let newRecord = Record(context: managedObjectContext)
-        let systolicInput = Int16(systolic!)
-        let diastolicInput = Int16(diastolic!)
+        guard let systolicInput = Int16(systolic) else {
+            print("Unable to convert the systolic input")
+            return
+        }
+        guard let diastolicInput = Int16(diastolic) else {
+            print("Unable to convert the systolic input")
+            return
+        }
         
         newRecord.systolic = systolicInput
         newRecord.diastolic = diastolicInput
